e7b566ea823329fd763bf8a9c5a8f59aad85fc02
Christopher Ivan <gunardi.ivan@gmail.com>
2021-02-23 21:12:29 +0700

    edit css

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -1,17 +1,12 @@
-import React from "react";
-import "./ComingSoon.scss";
+import React from 'react'
+import { H1, H3 } from '../components/Headings'
+import "./ComingSoon.scss"
 
 export const ComingSoon = () => {
-  return (
-    <div className="coming-soon">
-      <h1>COMING SOON</h1>
-      <h2>WISUDA APRIL ITB 2021</h2>
-      <h3>WISUDA APRIL ITB 2021</h3>
-      <h4>WISUDA APRIL ITB 2021</h4>
-      <h5>WISUDA APRIL ITB 2021</h5>
-      <h6>WISUDA APRIL ITB 2021</h6>
-      <a href="www.wispril.com">WISUDA APRIL ITB 2021</a>
-      <p>WISUDA APRIL ITB 2021</p>
-    </div>
-  );
-};
+    return (
+        <div className="coming-soon">
+            <H1>COMING SOON</H1>
+            <H3>WISUDA APRIL ITB 2021</H3>
+        </div>
+    )
+}
