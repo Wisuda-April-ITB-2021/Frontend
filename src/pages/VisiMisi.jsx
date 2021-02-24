05157d6bd7887edba93fa81374d15493763dbfe7
Annel Rashka Perdana <78416034+annelrashka@users.noreply.github.com>
2021-02-24 21:35:36 +0700

    Visi Misi Homepage

---
+++ src/pages/VisiMisi.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../pages/VisiMisi.scss";
+
+
+
+
+export const VisiMisi = () => {
+    return (
+        <div className="visi-misi">  
+            <header>
+            <h2>PERAYAAN WISUDA APRIL ITB 2021</h2>
+            <h4>“The Light of Hope in Times of Change”</h4>
+            </header>
+            <section className = "visi">
+                <h3> VISI </h3>
+                <h6> “Perayaan Wisuda April yang mengapresiasi purnastudi melalui <br />
+                     apresiasi terhadap perubahan potensi dengan berlandaskan <br />
+                      empati, kolaborasi, dan integrasi”</h6>
+            </section>
+            <section className = "misi">
+                <h3> MISI </h3>
+                <h6>
+                    <p>1. Menjadikan Perayaan Wisuda April 2021 sebagai wadah apresiasi <br />
+                         purnastudi melalui optimalisasi potensi yang hadir dalam <br />
+                         perubahan dimensi.<br /></p>
+                    <p>2. Mewadahi keberagaman elemen KM ITB lewat terciptanya wisuda <br />
+                         inklusif-integratif. Wisuda inklusif-integratif adalah perayaan yang <br />
+                         memperhitungkan kebutuhan wadah apresiasi dan optimalisasi diri <br />
+                         setiap elemen KM ITB.</p>
+                    <p>3. Mewadahi kebutuhan wisudawan tentang kebutuhan pasca kelulusan</p>
+                </h6>
+
+            </section>
+        </div>
+    );
+};
