7ac1423b594edc7fae1c6ccc5d7ce4102d6bf2a6
Josh <joshiatanumihardja@gmail.com>
2021-03-11 08:58:38 +0700

    remove unecessary clamp, fix padding

--- src/components/shared/List.jsx
+++ src/components/shared/List.jsx
@@ -9,22 +9,22 @@ import React from "react";
 import "./List.scss";
 
 export const List = (props) => {
-  return (
-    <ul className="list" {...props}>
-      {props.children}
-    </ul>
-  );
+    return (
+        <ul className="list" {...props}>
+            {props.children}
+        </ul>
+    );
 };
 
 export const ListItem = (props) => {
-  var { title, ...other } = props;
-  return (
-    <li className="listItem" {...other}>
-      <div className="bullet" />
-      <div className="listItemWrapper">
-        <h5>{title}</h5>
-        <p style={{ marginTop: "8px" }}>{props.children}</p>
-      </div>
-    </li>
-  );
+    var { title, ...other } = props;
+    return (
+        <li className="listItem" {...other}>
+            <div className="bullet" />
+            <div className="listItemWrapper" style={{ "paddingBottom": (props.children) ? "25px" : "10px" }}>
+                <h5>{title}</h5>
+                <p style={{ marginTop: "8px" }}>{props.children}</p>
+            </div>
+        </li>
+    );
 };
