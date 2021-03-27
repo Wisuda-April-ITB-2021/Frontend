dc2047e1fc96cbc26f40cf906202249f41781f37
Josh <joshiatanumihardja@gmail.com>
2021-03-28 00:51:22 +0700

    Finished base piccrew

---
+++ src/images/picrew/base/bg/index.js
@@ -0,0 +1,22 @@
+import BIRU from "./biru.jpg"
+import GADANG from "./gadang.jpg"
+import HIJAU from "./hijau.jpg"
+import KUCING from "./kucing.jpg"
+import KUNING from "./kuning.jpg"
+import LANGIT from "./langit.jpg"
+import MERAH from "./merah.jpg"
+import POHON from "./pohon.jpg"
+import SABUGA from "./sabuga.jpg"
+import SARAGA from "./saraga.jpg"
+export default BASE = [
+    BIRU, 
+    GADANG, 
+    HIJAU, 
+    KUCING, 
+    KUNING, 
+    LANGIT, 
+    MERAH, 
+    POHON, 
+    SABUGA, 
+    SARAGA
+]
