9ae8dc46840fc40db431ea6c17bb1925a084bdc4
M Syahrul Surya Putra <65522975+msyahrulsp@users.noreply.github.com>
2021-03-28 08:15:04 +0700

    accessories partially done

---
+++ src/images/picrew/accessories/etc/index.js
@@ -0,0 +1,23 @@
+import ANTING from "./anting.png"
+import CHOKER_BUNGA from "./choker-bunga.png"
+import FRECKLESS_1 from "./freckless-1.png"
+import FRECKLESS_2 from "./freckless-2.png"
+import FRECKLESS_3 from "./freckless-3.png"
+import JERAWAT from "./jerawat.png"
+import KALUNG_1 from "./kalung-1.png"
+import KALUNG_2 from "./kalung-2.png"
+import KALUNG_3 from "./kalung-3.png"
+import TINDIK_HIDUNG from "./tindik-hidung.png"
+
+export default ETC = [
+	ANTING,
+	CHOKER_BUNGA,
+	FRECKLESS_1,
+	FRECKLESS_2,
+	FRECKLESS_3,
+	JERAWAT,
+	KALUNG_1,
+	KALUNG_2,
+	KALUNG_3,
+	TINDIK_HIDUNG
+]
