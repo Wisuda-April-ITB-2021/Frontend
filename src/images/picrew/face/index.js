20fc503caa59ff0b0fe99a2950a4616ce7cdb51a
Azhar Ikhtiarudin <azharikhtiarudin@gmail.com>
2021-03-30 23:47:54 +0700

    add picrew face

--- src/images/picrew/face/index.js
+++ src/images/picrew/face/index.js
@@ -2,9 +2,19 @@
 import ALIS from './alis'
 import ETC from './etc'
 import HIDUNG from './hidung'
+import JANGGUT from './janggut'
+import MATA from './mata'
+import MULUT from './mulut'
+import TELINGA from './telinga'
+
 import ALIS_THUMB from './alis-min.png'
 import ETC_THUMB from './etc-min.png'
 import HIDUNG_THUMB from './hidung-min.png'
+import JANGGUT_THUMB from './janggut-min.png'
+import MATA_THUMB from './mata-min.png'
+import TELINGA_THUMB from './telinga-min.png'
+import MULUT_THUMB from './mulut-min.png'
+
 
 const FACE = [{
 	image: ALIS_THUMB,
@@ -15,6 +25,18 @@ const FACE = [{
 }, {
 	image: HIDUNG_THUMB,
 	assets: HIDUNG,
+}, {
+	image: JANGGUT_THUMB,
+	assets: JANGGUT,
+}, {
+	image: MATA_THUMB,
+	assets: MATA,
+}, {
+	image: MULUT_THUMB,
+	assets: MULUT,
+}, {
+	image: TELINGA_THUMB,
+	assets: TELINGA,
 }]
 
 
